2fa696f9ccbec152e304c829d039c373f08be606
leo <hermosilloleoncio@gmail.com>
2024-08-02 22:44:06 -0700

    Added some visuals

--- inventory_tracker/app/page.js
+++ inventory_tracker/app/page.js
@@ -1,9 +1,44 @@
-'use client '
+'use client'
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import {firestore} from "@/firebase"
-import { Box } from "@mui/material";
+import {Box, Typography} from "@mui/material";
 
 export default function Home() {
-  return <Box></Box>
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState([]);
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventory = []
+    docs.forEach(docs => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventory)
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+      {
+        inventory.forEach((item) => {
+          return (
+          <>
+            {item.name}
+            {item.count}
+          </>
+          )
+        })
+      }
+    </Box>
+  )
 }
