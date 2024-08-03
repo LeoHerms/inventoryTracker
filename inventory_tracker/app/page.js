c9d6c47e31bc7cc6340c3015be3c1b03c4c367ce
leo <hermosilloleoncio@gmail.com>
2024-08-03 14:47:20 -0700

    Aligned the buttons

--- inventory_tracker/app/page.js
+++ inventory_tracker/app/page.js
@@ -197,27 +197,31 @@ export default function Home() {
           </Typography>
         </Box>
       
-      <Stack width="800px" height="300px" spacing={2} overflow="auto">
+        <Stack width="800px" height="300px" spacing={2} overflow="auto">
         {
-          inventory.map(({name, quantity})=>(
+          inventory.map(({name, quantity}) => (
             <Box key={name} 
                  width="100%" 
                  minHeight="150px"
                  display="flex"
                  alignItems="center"
-                 justifyContent="space-between" 
+                 justifyContent="space-between"
                  bgcolor="f0f0f0"
-                 padding={5}
+                 padding={3}
             >
-              <Typography variant="h3" color="#333" textAlign="center">
+              <Typography variant="h3" color="#333" textAlign="center" flex="1">
                 {name.charAt(0).toUpperCase() + name.slice(1)}
               </Typography>
-              <Typography variant="h3" color="#332" textAlign="center">
+              <Typography variant="h3" color="#332" textAlign="center" flex="1">
                 {quantity}
               </Typography>
-              <Stack direction="row" spacing={2}>
-                <Button variant="contained" onClick={() => addItem(name)}>Add</Button>
-                <Button variant="contained" onClick={() => removeItem(name)}>Remove</Button>
+              <Stack direction="row" spacing={2} flex="1" justifyContent="flex-end">
+                <Button variant="contained" onClick={() => addItem(name)}>
+                  Add
+                </Button>
+                <Button variant="contained" onClick={() => removeItem(name)} disabled={quantity <= 0}>
+                  Remove
+                </Button>
               </Stack>
             </Box>
           ))
